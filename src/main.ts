3df584614b7a27772fa481292af38416e9d9aaf0
Andrew Koltyakov <koltyakov@gmail.com>
2018-02-17 19:38:57 +0300

    Code comments

--- src/main.ts
+++ src/main.ts
@@ -9,44 +9,51 @@ app.on('window-all-closed', () => {
 });
 
 app.on('ready', () => {
-  (new AuthConfig()).getContext()
-    .then(conf => {
-      return getAuth(conf.siteUrl, conf.authOptions)
-      .then(async (auth) => {
 
-        const url = conf.siteUrl + '/';
+  // Prompts or checks for locally stored auth creds
+  (new AuthConfig()).getContext().then(conf => {
 
-        const cookies = auth.headers.Cookie.split('; ')
-          .map(c => {
-            const index = c.indexOf('=');
-            const name = c.substring(0, index);
-            const value = c.substring(index + 1, c.length);
-            return { url, name, value };
-          });
+    // Authenticates to SharePoint with `node-sp-auth` library
+    return getAuth(conf.siteUrl, conf.authOptions).then(async (auth) => {
 
-        console.log(cookies);
+      const url = conf.siteUrl + '/';
 
-        for (const cookie of cookies) {
-          await setCookiePromise(session.defaultSession.cookies, cookie);
-        }
-
-        mainWindow = new BrowserWindow({ width: 1024, height: 768 });
-
-        mainWindow.loadURL(conf.siteUrl);
-
-        mainWindow.webContents.session.cookies.get({ url: conf.siteUrl }, (err, cookies) => {
-          if (err) {
-            return console.log(err);
-          }
-          console.log(cookies);
+      // Processing auth cookies
+      const cookies = auth.headers.Cookie.split('; ')
+        .map(c => {
+          const index = c.indexOf('=');
+          const name = c.substring(0, index);
+          const value = c.substring(index + 1, c.length);
+          return { url, name, value };
         });
 
+      console.log(cookies);
+
+      // Setting cookies to the session
+      for (const cookie of cookies) {
+        await setCookiePromise(session.defaultSession.cookies, cookie);
+      }
+
+      mainWindow = new BrowserWindow({ width: 1024, height: 768 });
+
+      mainWindow.loadURL(conf.siteUrl);
+
+      // Debug check of the cookies
+      /*
+      mainWindow.webContents.session.cookies.get({ url: conf.siteUrl }, (err, cookies) => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(cookies);
       });
-    })
-    .catch(error => {
-      console.log(error);
-      app.quit();
+      */
+
     });
+  })
+  .catch(error => {
+    console.log(error);
+    app.quit();
+  });
 });
 
 const setCookiePromise = (sessionCookies, cookie) => {
